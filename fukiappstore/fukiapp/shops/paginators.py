5a7f0af5a7c176b6ff1c02e8bed7a605e9d84ab6
van phuc <phucvan683@gmail.com>
2023-09-16 19:50:05 +0700

    Update

--- fukiappstore/fukiapp/shops/paginators.py
+++ fukiappstore/fukiapp/shops/paginators.py
@@ -1,7 +1,7 @@
 from rest_framework import pagination
 
 class ProductPaginator(pagination.PageNumberPagination):
-    page_size = 5
+    page_size = 10
 
 class ProductShopPaginator(pagination.PageNumberPagination):
     page_size = 10
